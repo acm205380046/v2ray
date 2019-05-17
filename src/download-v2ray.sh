08ea27de06f8d61576cd057bd4f1bd83a44d9097
acm205380046 <205380046@qq.com>
2019-05-17 09:38:45 +0800

    Update download-v2ray.sh

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -18,8 +18,7 @@ _download_v2ray_file() {
 	[[ -d /tmp/v2ray ]] && rm -rf /tmp/v2ray
 	mkdir -p /tmp/v2ray
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
-	v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
-
+	v2ray_download_link="https://disk-1252542619.cos.ap-chengdu.myqcloud.com/v2ray-linux-64.zip"
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
         $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
